bba3483b3733a0ae77578abe027cf208698cc13d
allemkarthik <allemkarthik@gmail.com>
2026-05-22 00:03:26 -0700

    NavBar + routers

---
+++ src/components/Body.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router";
+import NavBar from "./NavBar"
+const Body=()=>{
+   return (
+    
+    <div>
+        <NavBar />
+        <Outlet />
+    </div>
+   ) 
+}
+export default Body;
