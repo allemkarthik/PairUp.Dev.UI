8efff46cf20d2bc53370c1416578b9eae072d879
allemkarthik <allemkarthik@gmail.com>
2026-07-13 22:46:08 -0700

    payment verifications

--- src/pages/Premium.jsx
+++ src/pages/Premium.jsx
@@ -1,8 +1,31 @@
 import axios from "axios";
 import { BASE_URL } from "../utils/data";
-
+import { useEffect, useState } from "react";
 
 const Premium = () => {
+  const [isUserPremium, setIsUserPremium] = useState(false);
+
+  //make an api call for verifed payments
+  const verifyPremiumUser = async () => {
+    try {
+      const res = await axios.get(`${BASE_URL}/premium/verify`, {
+        withCredentials: true,
+      });
+
+      if (res.data.isPremium) {
+        setIsUserPremium(true);
+      }
+    } catch (err) {
+      console.error("Failed to verify premium status:", err);
+    }
+  };
+
+  //whenver page loads udpate if already a premium user
+
+  useEffect(() => {
+    verifyPremiumUser();
+  }, []);
+
   const handleBuy = async (type) => {
     const order = await axios.post(
       BASE_URL + "/payment/create",
@@ -12,7 +35,7 @@ const Premium = () => {
       { withCredentials: true },
     );
 
-    const {amount,keyId, currency, notes, orderId}= order.data
+    const { amount, keyId, currency, notes, orderId } = order.data;
     // Open Razorpay Checkout
     const options = {
       key: keyId, // Replace with your Razorpay key_id
@@ -23,10 +46,27 @@ const Premium = () => {
       order_id: orderId, // This is the order_id created in the backend
       prefill: {
         name: `${notes.firstName} ${notes.lastName}`,
-        email: notes.emailID,  
+        email: notes.emailID,
       },
       theme: {
         color: "#F37254",
+      },
+
+      //handler function for payment captured
+      //when the dialoug box is closed then the handler fun will called on sucess
+      handler: async function (res) {
+        try {
+          await axios.post(BASE_URL + "/payment/verify", res, {
+            withCredentials: true,
+          });
+
+          setIsUserPremium(true);
+
+          alert("🎉 Welcome to PairUpDev Premium!");
+        } catch (err) {
+          console.error(err);
+          alert("Payment verification failed.");
+        }
       },
     };
     //it should open the razarpay dialog box
@@ -34,6 +74,21 @@ const Premium = () => {
     rzp.open();
   };
 
+  if (isUserPremium) {
+    return (
+      <div className="flex justify-center items-center min-h-[70vh]">
+        <div className="card bg-success text-success-content shadow-xl w-96">
+          <div className="card-body text-center">
+            <h2 className="text-3xl font-bold">🎉 You're a Premium Member</h2>
+
+            <p className="mt-2">Thank you for supporting PairUpDev.</p>
+
+            <p>Enjoy all premium features 🚀</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="max-w-6xl mx-auto px-6 py-12">
       {/* Heading */}
